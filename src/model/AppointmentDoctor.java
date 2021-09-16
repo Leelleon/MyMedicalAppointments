893e9716d69cd2c8a81b803f89139921c585eb0e
Leelleon <leonardosantosbran@gmail.com>
2021-09-16 17:53:02 -0300

    Se implementa Interfaz en AppointmentDoctor, se agrega su metodo

--- src/model/AppointmentDoctor.java
+++ src/model/AppointmentDoctor.java
@@ -1,11 +1,15 @@
 package model;
 import java.util.Date;
 
-public class AppointmentDoctor {
+public class AppointmentDoctor implements ISchedulable{
 	private int id;
 	private Patient patient;
 	private Doctor doctor;
 	private Date date;
 	private String time;
 	
+	@Override
+	public void schedule(Date date, String time) {
+	
+	}
 }
